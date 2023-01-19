a4c31d975ae640f55dea962f70ee076fee6d00f6
Stacey <stacey6105@gmail.com>
2023-01-19 10:53:39 +0000

    Remove console.log in test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -329,7 +329,6 @@ describe('app', () => {
         test('Users array items are objects with keys for username, name and avatar_url', () => {
             return request(app).get('/api/users').expect(200).then((response) => {
                 const users = response.body.users
-                console.log(users);
                 users.forEach(user => {
                     expect(user).toHaveProperty('username')
                     expect(user).toHaveProperty('name')
