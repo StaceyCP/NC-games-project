14742d1015a05212c9b3df02b7fd8330399a3bd0
Stacey <stacey6105@gmail.com>
2023-01-20 18:02:33 +0000

    Refactor reviewsRouter.js

--- routes/reviewsRouter.js
+++ routes/reviewsRouter.js
@@ -1,10 +1,18 @@
 const { getReviews, getReviewById, getCommentsByReview_id, postCommentWithReview_id, patchReviewById } = require('../controllers/app_controller');
 const reviewsRouter = require('express').Router();
 
-reviewsRouter.get('/', getReviews)
-reviewsRouter.get('/:review_id', getReviewById)
-reviewsRouter.get('/:review_id/comments', getCommentsByReview_id)
-reviewsRouter.post('/:review_id/comments', postCommentWithReview_id)
-reviewsRouter.patch('/:review_id', patchReviewById)
+reviewsRouter
+.route('/')
+.get(getReviews)
+
+reviewsRouter
+.route('/:review_id')
+.get(getReviewById)
+.patch(patchReviewById)
+
+reviewsRouter
+.route('/:review_id/comments')
+.get(getCommentsByReview_id)
+.post(postCommentWithReview_id)
 
 module.exports = reviewsRouter
